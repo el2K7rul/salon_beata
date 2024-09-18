a1948d0431bc9908d2b6ceeeffa41140a17b1a57
Karol_O <elkarul@gmail.com>
2024-09-18 11:56:47 +0200

    added email validation

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,17 @@ window.onload = function () {
   //selectors
   const burger = document.querySelector(".burger");
   const allBurgerBars = document.querySelectorAll(".burger__bars");
+
   const navMobile = document.querySelector(".nav");
   const allNavLinks = document.querySelectorAll(".nav-mobile__link");
   const footerDate = document.querySelector(".footer__bottom--date");
   const allSections = document.querySelectorAll(".section");
+  const errorMsgMail = document.querySelector(".error-msg-mail");
+  const errorMsgTel = document.querySelector(".error-msg-tel");
+  const email = document.querySelector(".email");
+  const telNum = document.querySelector(".telNum");
 
-  console.log(allBurgerBars);
+  const submitBtn = document.querySelector(".submit");
 
   //variables
   const currentDate = new Date();
@@ -48,8 +53,55 @@ window.onload = function () {
     });
   };
 
+  // const checkTelNum = (telNum) => {
+  //   const validTel = /^[?<!\w][\[?[\+|00]?48\]?]?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}[?!\w]/;
+
+  //   if (validTel.test(telNum.value)) {
+  //     clearError(telNum);
+  //   } else {
+  //     showError(telNum, "wpisz poprawny nr tel.");
+  //   }
+  // };
+
+  const checkMail = (email) => {
+    const validMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if (validMail.test(email.value)) {
+      clearError(email);
+    } else {
+      showError(email, "wpisz poprawny adres email.");
+    }
+  };
+  const clearError = (el) => {
+    const formBox = el.errorMsgMail;
+    formBox.classList.remove("error-display");
+    errorMsgMail.textContent = "";
+  };
+
+  const showError = (input, msg) => {
+    errorMsgMail.classList.add("error-display");
+    errorMsgMail.textContent = msg;
+  };
+
+  const checkForm = (el) => {
+    if (el.value === "") {
+      showError(el, "wpisz mail");
+      showError(el, "wpisz tel");
+    } else {
+      // clearError(el);
+      checkMail(email);
+      checkTelNum(telNum);
+    }
+  };
+
   // event listeners
   window.addEventListener("scroll", handleObserver);
   burger.addEventListener("click", handleNav);
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    // checkForm(email, telNum);
+    checkMail(email);
+    checkTelNum(telNum);
+  });
   handleFooterYear();
 };
