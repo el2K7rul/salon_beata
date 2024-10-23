6a008de5a9e7a753915a826724c77dc3584dca25
Karol_O <elkarul@gmail.com>
2024-10-23 21:42:32 +0200

    email msg

--- src/js/main.js
+++ src/js/main.js
@@ -24,26 +24,26 @@ window.onload = function () {
   const acceptBtn = document.querySelector(".cookie-box__buttons--accept");
   const rejectBtn = document.querySelector(".cookie-box__buttons--reject");
 
-  acceptBtn.onclick = () => {
-    document.cookie = "CookieBy=salonbeata.eu; max-age=" + 60 * 60 * 24;
-    if (document.cookie) {
-      consentBox.classList.add("hide");
-    } else {
-      alert(
-        "Cookie can't be set! Please" +
-          " unblock this site from the cookie" +
-          " setting of your browser."
-      );
-    }
-  };
+  // acceptBtn.onclick = () => {
+  //   document.cookie = "CookieBy=salonbeata.eu; max-age=" + 60 * 60 * 24;
+  //   if (document.cookie) {
+  //     consentBox.classList.add("hide");
+  //   } else {
+  //     alert(
+  //       "Cookie can't be set! Please" +
+  //         " unblock this site from the cookie" +
+  //         " setting of your browser."
+  //     );
+  //   }
+  // };
 
-  rejectBtn.onclick = () => {
-    alert("Cookies rejected. Some functionality may be limited.");
-    consentBox.classList.add("hide");
-  };
+  // rejectBtn.onclick = () => {
+  //   alert("Cookies rejected. Some functionality may be limited.");
+  //   consentBox.classList.add("hide");
+  // };
 
-  let checkCookie = document.cookie.indexOf("CookieBy=GeeksForGeeks");
-  checkCookie !== -1 ? consentBox.classList.add("hide") : consentBox.classList.remove("hide");
+  // let checkCookie = document.cookie.indexOf("CookieBy=GeeksForGeeks");
+  // checkCookie !== -1 ? consentBox.classList.add("hide") : consentBox.classList.remove("hide");
 
   //send mail
 
